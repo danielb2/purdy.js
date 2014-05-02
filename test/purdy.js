873f801f61c8eb85e56e5d549b89b54e52c3c1f7
Daniel Bretoi <daniel@bretoi.com>
2014-05-02 00:26:54 -0700

    print objects too

--- test/purdy.js
+++ test/purdy.js
@@ -20,11 +20,8 @@ describe('Purdy', function() {
 
     it('should purdy print', function(done) {
 
-        Purdy('winter');
-        Purdy(123);
         Purdy([1,2,'foo', it, Array.isArray, new Date,1,1,1,1,12,[1,2]]);
-        Purdy({a: 3, b: 'foo', rain: it, d: {king: 'cobra'}});
-        Purdy(Date);
+        Purdy({a: 3, bn: 'foo', raino: it, d: {king: 'cobra'}});
         done();
     });
 });
