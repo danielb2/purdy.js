39893e87cc4f73128969029f4233fbcd750ce63e
Daniel Bretoi <daniel@bretoi.com>
2014-05-01 22:27:21 -0700

    inital commit

---
+++ test/purdy.js
@@ -0,0 +1,29 @@
+var Lab = require('lab');
+var Purdy = require('../');
+
+
+// Declare internals
+
+var internals = {};
+
+
+// Test shortcuts
+
+var expect = Lab.expect;
+var before = Lab.before;
+var after = Lab.after;
+var describe = Lab.experiment;
+var it = Lab.test;
+
+
+describe('Purdy', function() {
+
+    it('should purdy print', function(done) {
+
+        Purdy('winter');
+        Purdy(123);
+        Purdy([1,2,'foo']);
+        done();
+    });
+});
+
