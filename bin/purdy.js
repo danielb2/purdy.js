6e5c13ea3990d4a4807e5e8b27e2c1b6fa2b40aa
Daniel Bretoi <daniel@bretoi.com>
2015-11-26 15:19:13 +0800

    also handle stdin

--- bin/purdy.js
+++ bin/purdy.js
@@ -3,10 +3,56 @@
 var Fs = require('fs');
 var Purdy = require('../');
 
-try {
-    var path = Fs.realpathSync(process.argv[2]);
-    Purdy(require(path));
-}
-catch (e) {
-    Purdy(e);
-}
+
+var internals = {};
+
+
+internals.stdin = function(stream, callback){
+
+    var buf = '';
+    stream.setEncoding('utf8');
+
+    stream.on('data', function(s){ buf += s });
+
+    stream.on('end', function(){
+
+        callback(buf);
+    }).resume();
+};
+
+
+internals.parse = function (str) {
+
+    try {
+        Purdy(JSON.parse(str));
+    }
+    catch (e) {
+        Purdy(e);
+    }
+};
+
+
+internals.main = function () {
+
+    var stream = process.stdin;
+
+    try {
+        if (process.argv[2] === '-') {
+            stream = process.stdin;
+        }
+        else {
+            var path = Fs.realpathSync(process.argv[2]);
+            stream = Fs.createReadStream(path);
+        }
+        internals.stdin(stream, function (str) {
+
+            internals.parse(str);
+        });
+    }
+    catch (e) {
+        Purdy(e);
+    }
+};
+
+
+internals.main();
